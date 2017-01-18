8f58164f75c84effbb84fc6508f15ef2f5c95cb3
Tech Lead <techlead@assemble.vn>
2017-01-18 17:24:01 +0700

    add redux for app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TrackList from './components/TrackList';
 
+import {configureStore} from './store';
+import * as actions from './actions';
+
 const tracks = [
     {
         id: 1,
@@ -14,6 +17,9 @@ const tracks = [
         title: 'Cơn mưa ngang qua'
     }
 ];
+
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
 
 ReactDOM.render(
     <TrackList tracks={tracks} />,
