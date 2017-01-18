e22e42d7a0fb8ba02ea0798ec588b3e2b535aec3
Tech Lead <techlead@assemble.vn>
2017-01-18 16:46:53 +0700

    fisrt component

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,40 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.js'
+    ],
 
-  output: {
-    path: __dirname + '/build',
-    publicPath: '/',
-    filename: 'app.js'
-  },
+    output: {
+        path: __dirname + '/build',
+        publicPath: '/',
+        filename: 'app.js'
+    },
 
-  devServer: {
-    contentBase: './build'
-  },
+    devServer: {
+        contentBase: './build',
+        hot: true
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: true
-    })
-  ]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: true
+        })
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'react-hot!babel',
+                // query: {
+                //     presets: ['react', 'es2015']
+                // }
+            }
+        ]
+    }
 }
